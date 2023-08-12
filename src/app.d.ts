96b01fdee7a1918cc89ad4a0ae9b3d321741be60
gustavocadev <gustavocadev@gmail.com>
2023-08-11 19:48:20 -0500

    chore: update definitions

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,24 +1,24 @@
+/* eslint-disable no-var */
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			auth: import('lucia-auth').AuthRequest;
-		}
-		// interface PageData {}
-		// interface Platform {}
+/// <reference types="@sveltejs/kit" />
+declare namespace App {
+	// interface Error {}
+	interface Locals {
+		auth: import('lucia').AuthRequest;
 	}
-
-	declare namespace Lucia {
-		type Auth = import('$lib/server/lucia').Auth;
-		// we omit the id property because lucia automatically generate it for us when we create a user
-		type UserAttributes = {
-			username: string;
-			names: string;
-			last_names: string;
-		};
-	}
+	// interface PageData {}
+	// interface Platform {}
 }
 
-export {};
+/// <reference types="lucia" />
+declare namespace Lucia {
+	type Auth = import('$lib/server/lucia').Auth;
+	// we omit the id property because lucia automatically generate it for us when we create a user
+	type DatabaseUserAttributes = {
+		username: string;
+		names: string;
+		last_names: string;
+	};
+	type DatabaseSessionAttributes = {};
+}
